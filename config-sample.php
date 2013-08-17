c7db64c9c3cbc7e79849f70d09856dc3e4add9f7
ocean90 <dominikschilling+git@gmail.com>
2013-08-17 16:32:27 +0200

    Restore config-sample.php

---
+++ config-sample.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Gallery - A project for 'WPF - Moderne Webanwendungen' at
+ * Cologne University of Applied Sciences.
+ *
+ * @author    Dominik Schilling <dominik.schilling@smail.fh-koeln.de>
+ * @author    Laura Hermann
+ * @author    Dario Vizzaccaro
+ * @link      https://github.com/ocean90/wpfmw-gallery
+ * @license   MIT
+ */
+
+/*****
+ * CONFIG FILE
+ */
+
+// URL for the application
+$app->config->url = 'http://localhost';
+
+// If you want to install the application in a subdir
+// you have to change this value.
+// For each subdir + 1.
+// Example: http://localhost/gallery/ => offset = 1
+$app->config->segment_offset = 0;
+
+/*****
+ * Database settings
+ */
+
+// The name of the database
+$app->config->db->name   = '';
+
+// Database username
+$app->config->db->user   = '';
+
+// Database password
+$app->config->db->pw     = '';
+
+// Database hostname
+$app->config->db->host   = 'localhost';
