ea86fa516d5a8f54c4fffee56b1d0507a12ef6b9
Dominik Schilling <dominikschilling+git@gmail.com>
2013-08-29 00:07:19 +0200

    Pimp upload form: Only allow images and allow multiple selections

--- includes/views/upload/index.php
+++ includes/views/upload/index.php
@@ -4,15 +4,18 @@
 <h2>File Upload</h2>
 
 <blockquote class="pull-right">
-    <p>Today everything exists to end in a photograph.</p>
+	<p>Today everything exists to end in a photograph.</p>
   <small><cite title="Source Title">Susan Sontag</cite></small>
 </blockquote>
-<div class="form-group">
-    <label for="exampleInputFile">File input</label>
-    <input type="file" id="exampleInputFile">
-    <p class="help-block">Ein oder mehrere Bilder auswaehlen</p>
-  </div>
+
+<form action="" method="post" enctype="multipart/form-data">
+
+	<div class="form-group">
+		<label for="upload">File input</label>
+		<input type="file" id="upload" name="upload[]" multiple accept="image/*">
+		<p class="help-block">Choose one ore more pictures to upload</p>
+	</div>
+
+ </form>
+
 <?php include APP_VIEWS_PATH . 'footer.php'; ?>
-
-
-
