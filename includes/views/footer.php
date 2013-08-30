53c1802b7b78b33cae63f47bf921e9f66f387a31
loraenia <laura-hermann92@web.de>
2013-08-30 13:42:04 +0200

    footer

--- includes/views/footer.php
+++ includes/views/footer.php
@@ -5,5 +5,11 @@
 	if ( ! empty( $_['extra_footer'] ) )
 		echo $_['extra_footer'];
 	?>
+
+	<footer>
+      <div class="container">
+        <p>FOOOOOTER :-D <a href="#" rel="author">Authors</a>. Uses <a href="#" rel="external">Bla</a> and <a href="#" rel="external">Test</a>.</p>
+      </div>
+    </footer>
 	</body>
 </html>
