fde44589fee947636f6abbd7b6af1c04cc6efd6f
loraenia <laura-hermann92@web.de>
2013-08-30 18:34:41 +0200

    Revert "Merge branch 'master' of https://github.com/ocean90/wpfmw-gallery"
    This reverts commit 72be37cf69bbbb5103b2de3662f95f3320561707, reversing
    changes made to bba2b5a6488b02efb461abbe3c856f721ebfd750.

--- assets/js/image-uploader.js
+++ assets/js/image-uploader.js
@@ -2,65 +2,14 @@
 
 	var imageUploader = {
 		$el: $( '#image-uploader' ),
-		$button: $( '#image-upload-button' ),
 		$imagesToUpload: $( '#images' ),
 		$container: $( '#image-container' ),
 		currentImages: [],
 
 		init: function() {
 			//this.$el.on( 'submit', $.proxy( this.uploadRequest, this ) );
-			this.$button.on( 'click', $.proxy( this.triggerFileSelect, this ) );
 			this.$imagesToUpload.on( 'change', $.proxy( this.imagesChanged, this ) );
 			//this.$imagesToUpload.on( 'change', $.proxy( this.previewImages, this ) );
-		},
-
-		template: function( data ) {
-			return $( '<div/>' )
-				.attr( 'data-hash', data.hash )
-				.addClass( 'upload-image-wrapper clearfix' )
-				.append(
-					$( '<img/>' )
-						.addClass( 'img-thumbnail not-loaded' )
-						.attr( 'src', data.imageSrc )
-				)
-				.append(
-					$( '<div/>' )
-						.addClass( 'progress' )
-						.append(
-							$( '<div/>' )
-							.addClass( 'progress-bar' )
-							.width( '5%' )
-						)
-				)
-				.append(
-					$( '<div/>')
-						.addClass( 'form-group')
-						.append(
-							$( '<label/>')
-								.attr( 'for', 'image-description-' + data.hash )
-								.append(
-									'Description:'
-								)
-						)
-						.append(
-							$( '<textarea/>')
-								.addClass( 'form-control image-description' )
-								.attr( 'rows', 3 )
-								.attr( 'id', 'image-description-' + data.hash )
-						)
-				)
-				.append(
-					$( '<button/>')
-						.attr( 'type', 'button' )
-						.addClass( 'btn btn-danger btn-xs pull-right')
-						.html(
-							'Delete'
-						)
-				);
-		},
-
-		triggerFileSelect: function( e ) {
-			this.$imagesToUpload.trigger( 'click' );
 		},
 
 		imagesChanged: function( e ) {
@@ -89,8 +38,6 @@
 
 			$.each( images, function( index, value ) {
 				var reader = new FileReader();
-				var image = images[ index ];
-				var hash = md5( image.name + image.size + image.type );
 
 				reader.onload = function ( file ) {
 					var image = new Image();
@@ -98,45 +45,12 @@
 
 					image.onload = function() {
 						self.$container.prepend(
-							self.template( {
-								'hash' : hash,
-								imageSrc : this.src
-							} )
+							$( '<img/>' ).addClass( 'img-thumbnail image-to-upload' ).attr( 'src', this.src )
 						);
 					}
 				}
 
-				reader.readAsDataURL( image );
-			} );
-
-			// Call recalculateHeights after a small delay
-			setTimeout( self.recalculateHeights, 500 );
-		},
-
-		recalculateHeights: function() {
-			var i = 0, tmp = [], max = 0;
-
-			$( '.upload-image-wrapper' ).each( function() {
-				$el = $( this );
-				$el.height( 'auto' );
-				//console.log($el.outerHeight());
-				if ( i < 3 ) {
-					max = Math.max( max, $el.outerHeight() );
-					i = i + 1;
-					tmp.push( $el );
-				} else if ( i === 3 ) {
-					max = Math.max( max, $el.outerHeight() );
-					tmp.push( $el );
-
-					// we have 4 items
-					$.each( tmp, function( index, value ) {
-						value.height( max + 'px' );
-					} );
-
-					i = 0;
-					max = 0;
-					tmp = [];
-				}
+				reader.readAsDataURL( images[ index ] );
 			} );
 		},
 
@@ -147,16 +61,14 @@
 				var hash = md5( image.name + image.size + image.type );
 				var data = new FormData();
 				data.append( 'image', image);
-				data.append( 'hash', hash );
 				$.ajax({
 					url: ajaxURL + 'upload/',
 					data: data,
 					type: 'POST',
 					processData: false,
 					contentType: false,
-					dataType: 'json',
 					success: function( e ) {
-						var image = self.$container.find( '[data-hash="' + e.hash + '"]' );
+						console.log( e );
 					},
 					xhr: function() {
 						// Add a custom event listener for progress status
@@ -173,6 +85,7 @@
 		showProgress: function( e ) {
 			if ( e.lengthComputable ) {
 				var percentComplete = ( e.loaded / e.total ) * 100;
+				console.log(percentComplete);
 			}
 		}
 	}
--- includes/views/upload/index.php
+++ includes/views/upload/index.php
@@ -1,13 +1,11 @@
 <?php include APP_VIEWS_PATH . 'header.php'; ?>
 <?php include APP_VIEWS_PATH . 'navbar.php'; ?>
 
-<div class="page-header">
-	<h2>Upload <small>Create a new gallery</small></h2>
-</div>
+<h2>File Upload</h2>
 
 <blockquote class="pull-right">
 	<p>Today everything exists to end in a photograph.</p>
-	<small><cite>Susan Sontag</cite></small>
+  <small><cite title="Source Title">Susan Sontag</cite></small>
 </blockquote>
 
 <div id="image-uploader-wrapper">
@@ -15,9 +13,7 @@
 	 	<form id="image-uploader" action="" method="post" enctype="multipart/form-data">
 			<input type="file" id="images" name="images[]" multiple accept="image/*">
 
-			<button id="image-upload-button" type="button" class="btn btn-primary btn-lg btn-block">Select Images To Upload</button>
-
-			<div id="image-container" class="clearfix"></div>
+			<div id="image-container"></div>
 
 	 	</form>
 	 </div>
