df67cd282a5be3fcf3ee679acc0aeeaa4cacda19
Dominik Schilling <dominikschilling+git@gmail.com>
2013-09-02 12:38:32 +0200

    img-circle

--- includes/views/gallery/index.php
+++ includes/views/gallery/index.php
@@ -7,7 +7,7 @@
 		<?php
 		$creator = new User_Model( $_['gallery']->user_id );
 		if ( ! empty ( $creator->ID ) ) {
-			$image = '<img class="avatar" src="' . get_gravatar( $creator->user_email, 20 ) . '">';
+			$image = '<img class="img-circle avatar" src="' . get_gravatar( $creator->user_email, 20 ) . '">';
 			?>
 			<p>Creator: <a href="<?php site_url( 'user/' . $creator->user_login . '/' ); ?>"><?php echo $image . ' ' . $creator->user_login; ?></a></p>
 			<?php
